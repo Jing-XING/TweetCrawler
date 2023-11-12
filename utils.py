60b8c3de6528e4d17f53502a18fab50acdc5561b
xingjing <xjjsdsb@gmail.com>
2023-11-12 21:59:09 +0800

    optimize code

--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 
 def getXpath(browser,tweet):
-    # 使用JavaScript执行获取XPath的脚本
+    # use JavaScript execute the code getting XPath
     xpath = browser.execute_script("""
         function absoluteXPath(element) {
             var comp, comps = [];
@@ -55,30 +55,26 @@ def getXpath(browser,tweet):
 
 def splitTime(sinceTime,untilTime):
     #split untilTime-sinceTime by month
-    # 初始化一个列表来保存每个月的1号和月底日期
     dates = []
-    # 循环遍历两个日期之间的每个月
     for year in range(sinceTime.year, untilTime.year + 1):
-        for month in range(1, 13):  # 12个月
-            # 获取当前年份和月份的第一天和最后一天
+        for month in range(1, 13):
             first_day = datetime.datetime(year, month, 1)
             last_day = calendar.monthrange(year, month)[1]
             dates.append(first_day)
             dates.append(datetime.datetime(year, month, last_day))
-    # 输出日期列表
     s = dates[0::2]
     e = dates[1::2]
     sinceList = [str(date).split()[0] for date in s]
     untilList = [str(date).split()[0] for date in e]
     return sinceList,untilList
-def scrap(user_name,since_time,until_time,browser,advSearchComand):
+
+def scrap(browser,advSearchComand):
+    key='[data-testid="tweetText"]'#locating the tweets
     wait = WebDriverWait(browser, 10)
     browser.get(advSearchComand)
-    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="tweetText"]')))
+    WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, key)))
     collect=[]
-    # tweets=browser.find_elements(by=By.CSS_SELECTOR, value='[class="css-901oao css-cens5h r-18jsvk2 r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0"]')
-    tweets = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[data-testid="tweetText"]')))
-    # tweets = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[class="css-901oao css-cens5h r-18jsvk2 r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0"]')))
+    tweets = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, key)))
     while tweets:
         for tweet in tweets:
             if len(getXpath(browser,tweet))>=210:#过滤被转发微博，被转发微博xpath更长
@@ -86,9 +82,6 @@ def scrap(user_name,since_time,until_time,browser,advSearchComand):
             collect.append(tweet.text)
         browser.execute_script("arguments[0].scrollIntoView({ behavior: 'auto', block: 'start' });", tweet)#跳转到最后一条贴文
         time.sleep(5)
-        # WebDriverWait(browser, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-testid="tweetText"]')))
-        # element = wait.until(EC.visibility_of_element_located((By.ID, 'element_id')))
-        tempTweets = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, '[data-testid="tweetText"]')))
-        # tempTweets=wait.until(browser.find_elements(by=By.CSS_SELECTOR, value='[class="css-901oao css-cens5h r-18jsvk2 r-37j5jr r-a023e6 r-16dba41 r-rjixqe r-bcqeeo r-bnwqim r-qvutc0"]'))
+        tempTweets = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, key)))
         tweets=tempTweets[tempTweets.index(tweet)+1:]
     return collect
